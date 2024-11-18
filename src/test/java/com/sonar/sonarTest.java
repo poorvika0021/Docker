a992bf8644ee1e1e12936e36f82ee40a5f2048bc
Ubuntu <DockerPoo@DockerPoo.c4hjdnghy00u3dhvnlv5sohlve.ix.internal.cloudapp.net>
2024-11-18 13:01:32 +0000

    update

---
+++ src/test/java/com/sonar/sonarTest.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class sonarTest {
+
+    @Test
+    public void testSum() {
+        assertEquals(5, 2 + 3);
+    }
+}
+
